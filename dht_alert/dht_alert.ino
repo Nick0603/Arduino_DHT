d80741698013b31ad627d9d324dca82c28beb1d0
Nick <b10303008@gmail.com>
2016-06-21 15:25:09 +0800

    Add led_basic

--- dht_alert/dht_alert.ino
+++ dht_alert/dht_alert.ino
@@ -1,19 +1,24 @@
 #include <dht.h>
 #define dht_pin 5   //訊號從pin 5 進來
+#define led_pin 6   //LED 輸出的電位
 dht DHT;
 
 int Alert_Value = 60;
 
 void setup(){
   Serial.begin(9600);
+  pinMode(led_pin,OUTPUT);
   delay(1000);
 }
 
 void loop(){
   DHT.read11(dht_pin);    
   print_HT();
-  if(DHT.humidity > Alert_Value){
-    Serial.println("Warning! It's going to rain\n");
+  if(DHT.humidity > 40){
+    digitalWrite(led_pin,HIGH);
+    delay(100);
+    digitalWrite(led_pin,LOW);
+    delay(100);   
   }
   delay(1000);
 }
